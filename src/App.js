603fa8d55f0a5a041e6d7208ef84a5e9e926bf31
melvindionisio <melvinnudo.dionisio021@gmail.com>
2021-10-11 19:45:40 -0800

    clean up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const App = () => {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
-}
+  return <div className="App">Hello Worlds</div>;
+};
 
 export default App;
