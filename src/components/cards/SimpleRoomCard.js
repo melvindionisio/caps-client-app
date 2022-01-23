341e7f70628c2810050df21c4ccc8369d6715ffe
Melvin Dionisio <75345253+melvindionisio@users.noreply.github.com>
2022-01-23 23:42:31 +0800

    room image fix to the real image of room

--- src/components/cards/SimpleRoomCard.js
+++ src/components/cards/SimpleRoomCard.js
@@ -77,7 +77,8 @@ const SimpleRoomCard = ({ room }) => {
                            component="img"
                            height="130"
                            alt="room-image"
-                           image="https://images.unsplash.com/photo-1540518614846-7eded433c457?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2057&q=80"
+                           //image="https://images.unsplash.com/photo-1540518614846-7eded433c457?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2057&q=80"
+                           image={room.picture}
                         />
                      </CardActionArea>
                   </Box>
--- src/pages/RoomProfile.js
+++ src/pages/RoomProfile.js
@@ -132,9 +132,10 @@ const RoomProfile = (props) => {
                      <Card sx={{ borderRadius: 0, position: "relative" }}>
                         <CardMedia
                            component="img"
-                           height="200"
+                           height="250"
                            alt="room-image"
-                           image="https://images.unsplash.com/photo-1540518614846-7eded433c457?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2057&q=80"
+                           //image="https://images.unsplash.com/photo-1540518614846-7eded433c457?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2057&q=80"
+                           image={room.picture}
                         />
                         <CardHeader
                            sx={{
