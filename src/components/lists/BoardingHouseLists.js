7c1b0f3f6403ce290b95ef13e0606eaf45ac40ef
Melvin Dionisio <75345253+melvindionisio@users.noreply.github.com>
2022-02-19 14:32:25 +0800

    fix filter and sort button

--- src/components/lists/BoardingHouseLists.js
+++ src/components/lists/BoardingHouseLists.js
@@ -7,13 +7,17 @@ import Typography from "@mui/material/Typography";
 import MaleIcon from "@mui/icons-material/Male";
 import FemaleIcon from "@mui/icons-material/Female";
 //import WcIcon from "@mui/icons-material/Wc";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import { domain } from "../../fetch-url/fetchUrl";
 import {
    ToggleButton,
+   Button,
    Divider,
    Box,
    Grid,
    ToggleButtonGroup,
+   Slide,
 } from "@mui/material";
 
 const BoardingHouseLists = () => {
@@ -31,6 +35,9 @@ const BoardingHouseLists = () => {
    const [sortType, setSortType] = useState("desc");
    const [genderFilter, setGenderFilter] = useState("All");
    const [zoneFilter, setZoneFilter] = useState("All");
+
+   const [isFilterActive, setIsFilterActive] = useState(false);
+   const [isSortActive, setIsSortActive] = useState(false);
 
    const handleChangeZoneFilter = (event, newZoneFilter) => {
       setZoneFilter(newZoneFilter);
@@ -89,98 +96,146 @@ const BoardingHouseLists = () => {
 
    return (
       <Box sx={{ width: "100%" }}>
+         <Box
+            sx={{ display: "flex", gap: 1, mb: 1, justifyContent: "flex-end" }}
+         >
+            <Button
+               variant="outlined"
+               size="small"
+               startIcon={
+                  !isSortActive ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />
+               }
+               onClick={() => {
+                  setIsSortActive(!isSortActive);
+                  setIsFilterActive(false);
+               }}
+               color={!isSortActive ? "primary" : "secondary"}
+               disableElevation
+            >
+               Sort
+            </Button>
+            <Button
+               variant="outlined"
+               size="small"
+               onClick={() => {
+                  setIsFilterActive(!isFilterActive);
+                  setIsSortActive(false);
+               }}
+               color={!isFilterActive ? "primary" : "secondary"}
+               disableElevation
+               startIcon={
+                  !isFilterActive ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />
+               }
+            >
+               Filter
+            </Button>
+         </Box>
          <Grid container spacing={1}>
             <Grid item xs={12} sm={12} md={6}>
-               <Box
-                  sx={{
-                     display: "flex",
-                     justifyContent: "space-between",
-                  }}
-               >
-                  <ToggleButtonGroup
-                     color="primary"
-                     aria-label="sort"
-                     value={sort}
-                     size="small"
-                     exclusive
-                     onChange={handleChangeSort}
-                  >
-                     <ToggleButton
-                        value="bh_popularity"
-                        aria-label="sortbypopularity"
+               {isSortActive && (
+                  <Slide in={true} direction="right">
+                     <Box
+                        sx={{
+                           display: "flex",
+                           justifyContent: "space-between",
+                        }}
                      >
-                        Popularity
-                     </ToggleButton>
-                     <ToggleButton value="bh_name" aria-label="sortbyname">
-                        Name
-                     </ToggleButton>
-                  </ToggleButtonGroup>
-                  <ToggleButtonGroup
-                     color="primary"
-                     aria-label="sort"
-                     value={sortType}
-                     size="small"
-                     exclusive
-                     onChange={handleChangeSortType}
-                  >
-                     <ToggleButton value="asc" aria-label="sortbystar">
-                        ASC
-                     </ToggleButton>
-                     <ToggleButton value="desc" aria-label="sortbyname">
-                        DESC
-                     </ToggleButton>
-                  </ToggleButtonGroup>
-               </Box>
+                        <ToggleButtonGroup
+                           color="primary"
+                           aria-label="sort"
+                           value={sort}
+                           size="small"
+                           exclusive
+                           onChange={handleChangeSort}
+                        >
+                           <ToggleButton
+                              value="bh_popularity"
+                              aria-label="sortbypopularity"
+                           >
+                              Popularity
+                           </ToggleButton>
+                           <ToggleButton
+                              value="bh_name"
+                              aria-label="sortbyname"
+                           >
+                              Name
+                           </ToggleButton>
+                        </ToggleButtonGroup>
+                        <ToggleButtonGroup
+                           color="primary"
+                           aria-label="sort"
+                           value={sortType}
+                           size="small"
+                           exclusive
+                           onChange={handleChangeSortType}
+                        >
+                           <ToggleButton value="asc" aria-label="sortbystar">
+                              ASC
+                           </ToggleButton>
+                           <ToggleButton value="desc" aria-label="sortbyname">
+                              DESC
+                           </ToggleButton>
+                        </ToggleButtonGroup>
+                     </Box>
+                  </Slide>
+               )}
             </Grid>
             <Grid item xs={12} sm={12} md={6}>
-               <Box
-                  sx={{
-                     display: "flex",
-                     justifyContent: "space-between",
-                     mb: 1,
-                  }}
-               >
-                  <ToggleButtonGroup
-                     color="primary"
-                     aria-label="gender-sort"
-                     value={genderFilter}
-                     size="small"
-                     exclusive
-                     onChange={handleChangeGenderFilter}
-                  >
-                     <ToggleButton value="All" aria-label="sortbyall">
-                        All
-                     </ToggleButton>
-                     <ToggleButton value="Male" aria-label="sortbymale">
-                        <MaleIcon />
-                     </ToggleButton>
-                     <ToggleButton value="Female" aria-label="sortbyfemale">
-                        <FemaleIcon />
-                     </ToggleButton>
-                  </ToggleButtonGroup>
+               {isFilterActive && (
+                  <Slide in={true} direction="left">
+                     <Box
+                        sx={{
+                           display: "flex",
+                           justifyContent: "space-between",
+                           mb: 1,
+                        }}
+                     >
+                        <ToggleButtonGroup
+                           color="primary"
+                           aria-label="gender-sort"
+                           value={genderFilter}
+                           size="small"
+                           exclusive
+                           onChange={handleChangeGenderFilter}
+                        >
+                           <ToggleButton value="All" aria-label="sortbyall">
+                              All
+                           </ToggleButton>
+                           <ToggleButton value="Male" aria-label="sortbymale">
+                              <MaleIcon />
+                           </ToggleButton>
+                           <ToggleButton
+                              value="Female"
+                              aria-label="sortbyfemale"
+                           >
+                              <FemaleIcon />
+                           </ToggleButton>
+                        </ToggleButtonGroup>
 
-                  <ToggleButtonGroup
-                     color="primary"
-                     aria-label="gender-sort"
-                     value={zoneFilter}
-                     size="small"
-                     exclusive
-                     onChange={handleChangeZoneFilter}
-                  >
-                     <ToggleButton value="All" aria-label="all-zone">
-                        All
-                     </ToggleButton>
-                     <ToggleButton value="Zone 1" aria-label="zone1">
-                        Zone 1
-                     </ToggleButton>
-                     <ToggleButton value="Zone 2" aria-label="zone2">
-                        Zone 2
-                     </ToggleButton>
-                     <ToggleButton value="Zone 3" aria-label="zone3">
-                        Zone 3
-                     </ToggleButton>
-                  </ToggleButtonGroup>
-               </Box>
+                        <ToggleButtonGroup
+                           color="primary"
+                           aria-label="gender-sort"
+                           value={zoneFilter}
+                           size="small"
+                           exclusive
+                           onChange={handleChangeZoneFilter}
+                        >
+                           <ToggleButton value="All" aria-label="all-zone">
+                              All
+                           </ToggleButton>
+                           <ToggleButton value="Zone 1" aria-label="zone1">
+                              Zone 1
+                           </ToggleButton>
+                           <ToggleButton value="Zone 2" aria-label="zone2">
+                              Zone 2
+                           </ToggleButton>
+                           <ToggleButton value="Zone 3" aria-label="zone3">
+                              Zone 3
+                           </ToggleButton>
+                        </ToggleButtonGroup>
+                     </Box>
+                  </Slide>
+               )}
             </Grid>
          </Grid>
          <Divider sx={{ mb: 1 }} />
